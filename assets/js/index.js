fc79ffea41b62a6f4c331a38e8c1f19f02a10dbd
Eleazar Junsan <eleazarjunsan@Eleazars-Mac-mini.local>
2018-01-31 17:09:09 +0800

    Updated template files

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,1 +1,17 @@
-console.log('you can use ES6 here : )')
+$(function() {
+  $(window).scroll(function() {
+    if ($(this).scrollTop() > 63) /* .header-top height */ {
+      $('#menu').addClass('fixed');
+    } else {
+      $('#menu').removeClass('fixed');
+    }
+  });
+});
+
+
+$(document).ready(function(){
+
+  TweenMax.from('#slider-content span', 1.25, {opacity: "0", y: -20} )
+  TweenMax.from('#slider-content h1', 2, {x:30, opacity: "0", delay: 1.5})
+
+});
