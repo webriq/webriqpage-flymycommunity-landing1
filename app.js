d4f54ac4b6426b58301deadc3abee2f0815d31a9
Eleazar Junsan <eljunz@gmail.com>
2018-03-29 16:29:25 +0800

    Updated sharer link final

--- app.js
+++ app.js
@@ -4,12 +4,43 @@ const jsStandards = require('spike-js-standards')
 const pageId = require('spike-page-id')
 const Records = require('spike-records')
 const env = process.env.SPIKE_ENV
-
+const https = require('https')
+const fs = require('fs')
 const locals = {}
+
+
+fs.readFile('data/site.json', (err, data) => {
+  if(err) throw err;
+  let obj = JSON.parse(data);
+  let videoId = obj.content[0].videoid;
+  const url = 'https://vimeo.com/api/v2/video/'+videoId+'.json'
+  https.get(url, res => {
+    res.setEncoding("utf8");
+    let body = "";
+    res.on("data", data => {
+      body += data;
+    })
+    res.on("end", () => {
+      body = JSON.parse(body);
+      let videoLocation = body[0].thumbnail_large;
+      obj = {
+        "title": "vimeo",
+        "url": videoLocation
+      }
+      fs.writeFile('data/video.json', JSON.stringify(obj), (err) =>{
+        if(err) throw err;
+        console.log("File has been saved!")
+      })
+    })
+  })
+})
+
+
 
 const records = new Records({
   addDataTo: locals,
-  site: { file: 'data/site.json' }
+  site: { file: 'data/site.json' },
+  video: { file: 'data/video.json' }
 });
 
 module.exports = {
